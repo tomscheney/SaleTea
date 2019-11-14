4c1fbd5a7afcfe6373ce7553395969c03150c4a2
bang-start <1845077380@qq.com>
2019-11-14 09:51:47 +0800

    11.14 1

--- H5/js/product.js
+++ H5/js/product.js
@@ -1,6 +1,5 @@
 // 获取后台数据
 		$(document).ready(function() {
-			
 			// 获取地址栏中的字符串，并将其转化为对象
 			function addr_obj() {
 			 var search = location.search;
@@ -49,37 +48,8 @@
 				console.log(111)
 			}
 		});
-		$(document).ready(function() {
-			// function myFunction(){
-			console.log("dianji le ")
-			$.ajax({
-				url: "http://47.112.98.24:8090/getProductDetail",
-				type: "post",
-				dataType: "json",
-				data: {
-					'productId': '88880001', 
-				},
-				success: function(res) { // res就是后台接口返回的数据
-				console.log(res)
-					// 标题 价格
-					$(".tit-name").text(res.data.productDesc);
-					$(".tit-price").text(res.data.salePrice + '元 / 份');
-					// 产品信息
-					$(".productPlace").text(res.data.productDetail.productPlace);
-					$(".wrapping").text(res.data.productDetail.wrapping);
-					$(".brand").text(res.data.productDetail.brand);
-					$(".qualityPeriod").text(res.data.productDetail.qualityPeriod);
-					$(".standards").text(res.data.productDetail.standards);
-					// 产品亮点
-					$(".Bright-spot-1-r>span").text(res.data.productFeatures.featureDesc)
-				},
-			})
-			error: function data() {
-				console.log("dataerro", data);
-			}
-		});
-	
-	
+		
+
 
 		function adCar() {
 			location.href = "adress.html";
