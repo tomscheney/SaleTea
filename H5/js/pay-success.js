6dc9dfe3f55231146a13b8bb2ea6467f29dd5e4b
bang-start <1845077380@qq.com>
2019-11-18 10:18:18 +0800

    pay canshu

--- H5/js/pay-success.js
+++ H5/js/pay-success.js
@@ -75,18 +75,7 @@
 									   }
 								   ); 
 								}
-					  // function pay(){
-									if (typeof WeixinJSBridge == "undefined"){
-									   if( document.addEventListener ){
-									       document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-									   }else if (document.attachEvent){
-									       document.attachEvent('WeixinJSBridgeReady', onBridgeReady); 
-									       document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-									   }
-									}else{
-									   onBridgeReady();
-									} 	
-					  // }
+				
 				},
 			})
 			error: function data() {
