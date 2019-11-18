0bc8bba6fd297a3728365fa6fa4b1c73a290940f
bang-start <1845077380@qq.com>
2019-11-18 17:32:02 +0800

    alert pay

--- H5/js/paySuccess.js
+++ H5/js/paySuccess.js
@@ -55,28 +55,7 @@
 			console.log(res.code)
 			aPay = res.data,
 			console.log("aPAy", aPay)
-           alert(aPay.package);
-           alert(aPay.appId);
-           alert(aPay.timeStamp);
-           alert(aPay.nonceStr);
-           alert(aPay.signTyp);
-           alert(aPay.paySign);
-
-           WeixinJSBridge.invoke(
-                                 'getBrandWCPayRequest', {
-                                 "appId":aPay.appId, //公众号名称，由商户传入
-                                 "timeStamp": aPay.timeStamp, //时间戳，自1970年以来的秒数
-                                 "nonceStr": aPay.nonceStr, //随机串
-                                 "package": aPay.package,
-                                 "signType": aPay.signTyp, //微信签名方式:
-                                 "paySign": aPay.paySign //微信签名
-                                 },
-                                 function(res) {
-                                 if (res.err_msg == "get_brand_wcpay_request:ok") {
-                                 alert("支付成功");
-                                 } // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
-                                 }
-                                 );
+          
 		},
 	})
 	error: function data() {
@@ -88,9 +67,15 @@
 // });
 	// 支付调用
 	function onBridgeReady() {
+		alert(aPay.package);
+		alert(aPay.appId);
+		alert(aPay.timeStamp);
+		alert(aPay.nonceStr);
+		alert(aPay.signTyp);
+		alert(aPay.paySign);
 		WeixinJSBridge.invoke(
 			'getBrandWCPayRequest', {
-				"appId":aPay.appId, //公众号名称，由商户传入     
+				"appid":aPay.appId, //公众号名称，由商户传入     
 				"timeStamp": aPay.timeStamp, //时间戳，自1970年以来的秒数     
 				"nonceStr": aPay.nonceStr, //随机串     
 				"package": aPay.package,
