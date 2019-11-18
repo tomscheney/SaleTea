27bc11ebf4c252a7fe3d80d5084acbaea9821156
bang-start <1845077380@qq.com>
2019-11-18 12:20:13 +0800

    pay 10

--- H5/js/paySuccess.js
+++ H5/js/paySuccess.js
@@ -50,7 +50,36 @@
 				},
 				success: function(res) { // res就是后台接口返回的数据
 				    console.log(res)
-					console.log(res.code)			
+					console.log(res.code)	
+							function onBridgeReady() {
+								WeixinJSBridge.invoke(
+									'getBrandWCPayRequest', {
+										"appId": res.data.appId, //公众号名称，由商户传入     
+										"timeStamp": res.data.timeStamp, //时间戳，自1970年以来的秒数     
+										"nonceStr": res.data.nonceStr, //随机串     
+										"package": res.data.package,
+										"signType": res.data.signType, //微信签名方式:     
+										"paySign": res.data.paySign //微信签名 
+									},
+									function(res) {
+										if (res.err_msg == "get_brand_wcpay_request:ok") {
+											alert("支付成功");
+										} // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
+									}
+								);
+								}
+								function pay() {
+									if (typeof WeixinJSBridge == "undefined") {
+										if (document.addEventListener) {
+											document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+										} else if (document.attachEvent) {
+											document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+											document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+										}
+									} else {
+										onBridgeReady();
+									}
+								}
 				},
 			})
 			error: function data() {
