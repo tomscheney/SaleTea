d28618803fce30a833678a930e6453e3ce1b2f67
tomscheney <417497299@qq.com>
2019-11-18 16:26:46 +0800

    1.支付测试

--- H5/js/paySuccess.js
+++ H5/js/paySuccess.js
@@ -55,6 +55,22 @@
 			console.log(res.code)
 			aPay = res.data,
 			console.log("aPAy", aPay)
+           
+           WeixinJSBridge.invoke(
+                                 'getBrandWCPayRequest', {
+                                 "appId":aPay.appId, //公众号名称，由商户传入
+                                 "timeStamp": aPay.timeStamp, //时间戳，自1970年以来的秒数
+                                 "nonceStr": aPay.nonceStr, //随机串
+                                 "package": aPay.package,
+                                 "signType": aPay.signTyp, //微信签名方式:
+                                 "paySign": aPay.paySign //微信签名
+                                 },
+                                 function(res) {
+                                 if (res.err_msg == "get_brand_wcpay_request:ok") {
+                                 alert("支付成功");
+                                 } // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+                                 }
+                                 );
 		},
 	})
 	error: function data() {
