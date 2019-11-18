19ccc2d5fc387ebdec463c0c3b8912fe5f37b332
tomscheney <417497299@qq.com>
2019-11-18 22:59:15 +0800

    xxxxxxcc

--- H5/js/paySuccess.js
+++ H5/js/paySuccess.js
@@ -28,6 +28,29 @@
 			'&redirect_uri=https://zxz.kidstoms.com/H5/paySuccess.html&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect';
 
 	}
+
+// 支付调用
+function onBridgeReady() {
+    console.log(appId);
+    console.log(aPay.appId);
+    console.log("appId",appId);
+    WeixinJSBridge.invoke(
+                          'getBrandWCPayRequest', {
+                          "appId": aPay.appId, //公众号名称，由商户传入
+                          "timeStamp": aPay.timeStamp, //时间戳，自1970年以来的秒数
+                          "nonceStr": aPay.nonceStr, //随机串
+                          "package": aPay.package,
+                          "signType": aPay.signTyp, //微信签名方式:
+                          "paySign": aPay.paySign //微信签名
+                          },
+                          function(res) {
+                          if (res.err_msg == "get_brand_wcpay_request:ok") {
+                          alert("支付成功");
+                          } // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+                          }
+                          );
+}
+
 	console.log(obj.code)
 
 	var Fee = parseInt(Math.random() * (50 - 0 + 1) + 50);
@@ -63,6 +86,7 @@
 				var resu = JSON.parse(res);
 				console.log(resu)
 		        alert("ajx",JSON.stringify(res.code))
+               onBridgeReady();
 			},
 			error: function data() {
 				console.log("dataerro", data);
@@ -71,71 +95,7 @@
 			}
 		});
 		
-	console.log(aPay);
 
-// });
-	// 支付调用
-	function onBridgeReady() {
-	
-        var Fee = parseInt(Math.random() * (50 - 0 + 1) + 50);
-        function randomn(n) {
-            if (n > 10) return null
-                return parseInt((Math.random() + 1) * Math.pow(10, n - 1))
-                }
-        console.log(randomn(10))
-        var orderN = randomn(10);
-        var notifyUrl = "https://zxz.kidstoms.com/H5/paySuccess.html";
-        console.log("notifyUrl", notifyUrl)
-        var aPay = "";
-        $.ajax({
-               url: "https://47.112.98.24/getPayInfo",
-               type: "post",
-               // contentType: "application/json",
-               dataType: "json",
-               data: {
-               code: obj.code,
-               totalFee: Fee,
-               orderNo: orderN,
-               notifyUrl: notifyUrl,
-               },
-               success: function(res) { // res就是后台接口返回的数据
-               console.log(res)
-               console.log(res.code)
-               aPay = res.data,
-               console.log("aPAy", aPay)
-               alert("ajx",JSON.stringify(res.code))
-               
-               alert(aPay.package);
-               alert(aPay.appId);
-               alert(aPay.timeStamp);
-               alert(aPay.nonceStr);
-               alert(aPay.signType);
-               alert(aPay.paySign);
-               WeixinJSBridge.invoke(
-                                     'getBrandWCPayRequest', {
-                                     "appid":aPay.appId, //公众号名称，由商户传入
-                                     "timeStamp": aPay.timeStamp, //时间戳，自1970年以来的秒数
-                                     "nonceStr": aPay.nonceStr, //随机串
-                                     "package": aPay.package,
-                                     "signType": aPay.signType, //微信签名方式:
-                                     "paySign": aPay.paySign //微信签名
-                                     },
-                                     function(res) {
-                                     if (res.err_msg == "get_brand_wcpay_request:ok") {
-                                     alert("支付成功");
-                                     } // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
-                                     }
-                                     );
-               },
-               error: function data() {
-               console.log("dataerro", data);
-               console.log(111)
-               alert("ajaxerro")
-               }
-               })
-        
-        
-        
-		
-	}
+
+
 	
