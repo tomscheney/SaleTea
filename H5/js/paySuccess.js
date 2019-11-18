10609bb0113acf3ae5d30b17a407b2a34dba3963
bang-start <1845077380@qq.com>
2019-11-18 13:54:31 +0800

    pay3

--- H5/js/paySuccess.js
+++ H5/js/paySuccess.js
@@ -64,20 +64,21 @@
 			function onBridgeReady() {
 				WeixinJSBridge.invoke(
 					'getBrandWCPayRequest', {
-						appId: aPay.appId, //公众号名称，由商户传入     
-						timeStamp: aPay.timeStamp, //时间戳，自1970年以来的秒数     
-						nonceStr: aPay.nonceStr, //随机串     
-						package: aPay.package,
-						signType: aPay.signType, //微信签名方式:     
-						paySign: aPay.paySign //微信签名 
+						"appId": aPay.appId, //公众号名称，由商户传入     
+						"timeStamp": aPay.timeStamp, //时间戳，自1970年以来的秒数     
+						"nonceStr": aPay.nonceStr, //随机串     
+						"package": aPay.package,
+						"signType": aPay.signType, //微信签名方式:     
+						"paySign": aPay.paySign //微信签名 
 					},
+					console.log(appId)
 					function(res) {
 						if (res.err_msg == "get_brand_wcpay_request:ok") {
 							alert("支付成功");
 						} // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
 					}
 				);
-				console.log(appId,1)
+			
 			}
 			
 			console.log(appId,2)
