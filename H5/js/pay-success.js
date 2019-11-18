112b22aee29caf8a019195c5d59922cdefa5fe84
bang-start <1845077380@qq.com>
2019-11-18 11:30:47 +0800

    pay 4

--- H5/js/pay-success.js
+++ H5/js/pay-success.js
@@ -19,11 +19,11 @@
 			 console.log(obj.code)
 			if(obj.code == ''|| obj.code == undefined || obj.code == 'null') {
 				console.log("123")
-				var appid = 'wx6e974f12e898a2ee';
+				var appId = 'wx6e974f12e898a2ee';
 				// https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6e974f12e898a2ee&redirect_uri=http://zxz.kidstoms.com/H5/product.html
 				// &response_type=code&scope=snsapi_base&state=STATE#wechat_redirect
-				window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' 
-				+ appid + '&redirect_uri=https://zxz.kidstoms.com/H5/pay-success.html&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect';
+				window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appId=' 
+				+ appId + '&redirect_uri=https://zxz.kidstoms.com/H5/pay-success.html&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect';
 			
 			}
 			console.log(obj.code)
@@ -48,34 +48,7 @@
 				},
 				success: function(res) { // res就是后台接口返回的数据
 				    console.log(res)
-					console.log(res.code)
-					console.log(res.data.appId)
-					console.log(res.data.package)
-					function onBridgeReady() {
-								   WeixinJSBridge.invoke(
-								       'getBrandWCPayRequest', {
-								           // "appId" : "wx6e974f12e898a2ee",     //公众号名称，由商户传入     
-								           // "timeStamp": res.timeStamp,         //时间戳，自1970年以来的秒数     
-								           // "nonceStr" : res.randomString(), //随机串     
-								           // "package" : package,     
-								           // "signType" : signType,         //微信签名方式:     
-								           // "paySign" : paySign    //微信签名 
-										   "timeStamp": "1573832901",
-											"package": "prepay_id=wx15234821811451057aaa58961438304900",
-											"paySign": "2F4B8D4BBA034E0A6AE53733AD86367E",
-											"appId": "wx6e974f12e898a2ee",
-											"signType": "MD5",
-											"nonceStr": "NWxR0WvjNUEt7LTORmTULpCoaH6I7ciI"
-								       },
-								       
-								       function(res){     
-								           if(res.err_msg == "get_brand_wcpay_request:ok" ) {
-								        	   alert("支付成功");
-								           }  
-									   }
-								   ); 
-								}
-				
+					console.log(res.code)			
 				},
 			})
 			error: function data() {
