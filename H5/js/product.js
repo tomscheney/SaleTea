e32e64a8b498cbd1b4623a9b24b943a48a7d4d89
bang-start <1845077380@qq.com>
2019-11-21 13:05:12 +0800

    ada

--- H5/js/product.js
+++ H5/js/product.js
@@ -47,7 +47,15 @@
 				success: function(res) { // res就是后台接口返回的数据
 					console.log(res.code)
 					console.log(res)
-					alert(1)
+					alert(1);
+					wx.config({
+					  debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+					  appId: 'res.data.appId', // 必填，公众号的唯一标识
+					  timestamp: , // 必填，生成签名的时间戳
+					  nonceStr: '', // 必填，生成签名的随机串
+					  signature: '',// 必填，签名
+					  jsApiList: ["https://zxz.kidstoms.com/getAllProduct"] // 必填，需要使用的JS接口列表
+					});
 				},
 			})
 			error: function data() {
