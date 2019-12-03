359bd26e24e40928846d3339dfc3d677a79e1d83
wangsiyuan <wsy1005@163.com>
2019-12-03 15:05:23 +0800

    修改代码

--- H5/js/home.js
+++ H5/js/home.js
@@ -1,88 +1,88 @@
 // // 方法
-// function first() {
-//   // 获取地址栏中的字符串，并将其转化为对象
-//   function addr_obj() {
-//     let search = location.search;
-//     cosnole.log(search);
-//     let obj = {};
-//     cnsole.log(obj);
-//     let keyValues = search.slice(1).split("&");
-//     console.log(keyValues);
-//     keyValues.forEach(function(keyValue) {
-//       let tempArr = keyValue.split("=");
-//       let key = tempArr[0];
-//       // var value = tempArr[1].indexOf("|") > 0 ? tempArr[1].split("|") : tempArr[1];
-//       let value = tempArr[1];
-//       obj[key] = value;
-//     });
-//     return obj;
-//   }
-//   let obj = addr_obj();
-//   console.log(obj.code);
-//   //code 无值，
-//   if (obj.code == "" || obj.code == undefined || obj.code == "null") {
-//     console.log("123");
-//     let appid = "wx6e974f12e898a2ee";
-//     let redirect_uri = "https://kidstoms.com/tea/index.html";
-//     let response_type = "code";
-//     let state = "STATE#wechat_redirect";
-//     let scope = "snsapi_base";
-//     let result =
-//       "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" +
-//       appid +
-//       "&redirect_uri=" +
-//       redirect_uri +
-//       "&response_type=" +
-//       response_type +
-//       "&scope=" +
-//       scope +
-//       "&state=" +
-//       state;
-//     window.location.href = result;
-//     return;
-//   }
-//   console.log(obj.code);
-//   let code = obj.code;
-//   // 获取
-//   let openId = window.localStorage.getItem("openId");
+function first() {
+  // 获取地址栏中的字符串，并将其转化为对象
+  function addr_obj() {
+    let search = location.search;
+    cosnole.log(search);
+    let obj = {};
+    cnsole.log(obj);
+    let keyValues = search.slice(1).split("&");
+    console.log(keyValues);
+    keyValues.forEach(function(keyValue) {
+      let tempArr = keyValue.split("=");
+      let key = tempArr[0];
+      // var value = tempArr[1].indexOf("|") > 0 ? tempArr[1].split("|") : tempArr[1];
+      let value = tempArr[1];
+      obj[key] = value;
+    });
+    return obj;
+  }
+  let obj = addr_obj();
+  console.log(obj.code);
+  //code 无值，
+  if (obj.code == "" || obj.code == undefined || obj.code == "null") {
+    console.log("123");
+    let appid = "wx6e974f12e898a2ee";
+    let redirect_uri = "https://kidstoms.com/tea/H5/home.html";
+    let response_type = "code";
+    let state = "STATE#wechat_redirect";
+    let scope = "snsapi_base";
+    let result =
+      "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" +
+      appid +
+      "&redirect_uri=" +
+      redirect_uri +
+      "&response_type=" +
+      response_type +
+      "&scope=" +
+      scope +
+      "&state=" +
+      state;
+    window.location.href = result;
+    return;
+  }
+  console.log(obj.code);
+  let code = obj.code;
+  // 获取
+  let openId = window.localStorage.getItem("openId");
 
-//   // 判断openId是否存在
-//   if (openId == "" || openId == undefined || openId == "null") {
-//     openId();
-//   } else {
-//     window.location.href = "H5/home.html";
-//   }
-//   console.log(code);
+  // 判断openId是否存在
+  if (openId == "" || openId == undefined || openId == "null") {
+    openId();
+  } else {
+    window.location.href = "H5/home.html";
+  }
+  console.log(code);
 
-//   function getopenId() {
-//     console.log(code);
-//     // 查询openId
-//     $.ajax({
-//       url: "https://kidstoms.com/getOpenIdByCode",
-//       type: "get",
-//       dataType: "json",
-//       data: {
-//         code: code
-//       },
-//       success: function(res) {
-//         if (res.code === 200) {
-//           // alert("openid" + JSON.stringify(res.data.openId));
-//           let result = jQuery.parseJSON(res.data);
-//           var openId = result.openId;
-//           console.log(openId);
-//           window.localStorage.setItem("openId", openId);
-//           window.location.href = "H5/product.html?openId=" + openId;
-//           alert("openid:" + result.openId);
-//         } else {
-//           alert(res.msg);
-//         }
-//       },
-//       error: function() {
-//         console.log("XMLHttpRequest", XMLHttpRequest);
-//       }
-//     });
-//   }
-// }
+  function getopenId() {
+    console.log(code);
+    // 查询openId
+    $.ajax({
+      url: "https://kidstoms.com/getOpenIdByCode",
+      type: "get",
+      dataType: "json",
+      data: {
+        code: code
+      },
+      success: function(res) {
+        if (res.code === 200) {
+          // alert("openid" + JSON.stringify(res.data.openId));
+          let result = jQuery.parseJSON(res.data);
+          var openId = result.openId;
+          console.log(openId);
+          window.localStorage.setItem("openId", openId);
+          window.location.href = "H5/product.html?openId=" + openId;
+          alert("openid:" + result.openId);
+        } else {
+          alert(res.msg);
+        }
+      },
+      error: function() {
+        console.log("XMLHttpRequest", XMLHttpRequest);
+      }
+    });
+  }
+}
 
 // 获取全部数据
 function getProductList() {
