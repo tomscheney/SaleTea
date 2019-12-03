03ed89c12dd4fd3db7a789a369993fa47fc2659a
wangsiyuan <wsy1005@163.com>
2019-12-03 23:53:34 +0800

    修改代码

--- H5/js/cart.js
+++ H5/js/cart.js
@@ -61,8 +61,11 @@ $(".plus").click(function() {
 // 点击商品按钮
 $(".GoodsCheck").click(function() {
 	var goods = $(this).closest(".one-shop").find(".GoodsCheck"); //获取本店铺的所有商品
+	console.log(goods)
 	var goodsC = $(this).closest(".one-shop").find(".GoodsCheck:checked"); //获取本店铺所有被选中的商品
+	console.log(goodsC)
 	var Shops = $(this).closest(".one-shop").find(".ShopCheck"); //获取本店铺的全选按钮
+	console.log(Shops)
 	if (goods.length == goodsC.length) { //如果选中的商品等于所有商品
 		Shops.prop('checked', true); //店铺全选按钮被选中
 		if ($(".ShopCheck").length == $(".ShopCheck:checked").length) { //如果店铺被选中的数量等于所有店铺的数量
@@ -82,7 +85,8 @@ $(".GoodsCheck").click(function() {
 });
 // 点击店铺按钮
 $(".ShopCheck").change(function() {
-	if ($(this).prop("checked") == true) { //如果店铺按钮被选中
+	if ($(this).prop("checked") == true) { 
+		//如果店铺按钮被选中
 		$(this).parents(".one-shop").find(".goods-check").prop('checked', true); //店铺内的所有商品按钮也被选中
 		if ($(".ShopCheck").length == $(".ShopCheck:checked").length) { //如果店铺被选中的数量等于所有店铺的数量
 			$("#AllCheck").prop('checked', true); //全选按钮被选中
@@ -91,7 +95,8 @@ $(".ShopCheck").change(function() {
 			$("#AllCheck").prop('checked', false); //else全选按钮不被选中
 			TotalPrice();
 		}
-	} else { //如果店铺按钮不被选中
+	} else { 
+		//如果店铺按钮不被选中
 		$(this).parents(".one-shop").find(".goods-check").prop('checked', false); //店铺内的所有商品也不被全选
 		$("#AllCheck").prop('checked', false); //全选按钮也不被选中
 		TotalPrice();
@@ -123,6 +128,7 @@ function TotalPrice() {
 			$(this).closest(".one-shop").find(".ShopTotal").text(oprice.toFixed(2)); //显示被选中商品的店铺总价
 		});
 		var oneprice = parseFloat($(this).find(".ShopTotal").text()); //得到每个店铺的总价
+		console.log(oneprice)
 		allprice += oneprice; //计算所有店铺的总价
 	});
 	$("#AllTotal").text(allprice.toFixed(2)); //输出全部总价
