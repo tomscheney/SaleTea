6d0e33acbcf2e2e3a169bcd28744ebad6f838d32
tomscheney <417497299@qq.com>
2019-12-04 22:39:08 +0800

    reset .

--- H5/js/home.js
+++ H5/js/home.js
@@ -1,64 +1,69 @@
 // 获取全部数据
 function getProductList() {
-  // 获取地址栏中的字符串，并将其转化为对象
-  let openId = window.localStorage.getItem("openId");
-  $.ajax({
-    url: "https://kidstoms.com/getAllProduct",
-    type: "post",
-    dataType: "json",
-    data: {},
-    success: function(res) {
-      // res就是后台接口返回的数据
-      console.log(res);
-      console.log(res.data);
-      // 标题
-      var products = res.data;
-      var html = "";
-      for (var i = 0; i < products.length; i++) {
-        var product = products[i];
-        var imgI = product.productImages[0];
-        html +=
-          '<li class="tea" id=' +
-          product.productId +
-          ">" +
-          '<div class="tea-img">' +
-          "<img src=" +
-          imgI +
-          "/>" +
-          "</div>" +
-          '<p class="tea_title">' +
-          product.productName +
-          "</p>" +
-          '<p class="tea_contect">' +
-          product.productDesc +
-          "</p>" +
-          '<hr class="hr-1">' +
-          "</li>";
-        // for ( k=0;k<proId.length;k++){
-        // 	console.log(proId[k])
-        // }
-      }
-      $(".product_list>ul").html(html);
-      $(".product_list>ul>li").click(function(e) {
-        console.log("qqqqqqqq");
-        console.log(e.currentTarget.id);
-        window.location.href = "product.html?productId=" + e.currentTarget.id;
-      });
-    },
-    error: function data() {
-      console.log("dataerro", data);
-    }
-  });
+    // 获取地址栏中的字符串，并将其转化为对象
+    let openId = window.localStorage.getItem("openId");
+    $.ajax({
+        url: "https://kidstoms.com/getAllProduct",
+        type: "post",
+        dataType: "json",
+        data: {},
+        success: function (res) {
+            // res就是后台接口返回的数据
+            console.log(res);
+            console.log(res.data);
+            // 标题
+            var products = res.data;
+            var html = "";
+            for (var i = 0; i < products.length; i++) {
+                var product = products[i];
+                var imgI = product.productImages[0];
+                html +=
+                    '<li class="tea" id=' +
+                    product.productId +
+                    ">" +
+                    '<div class="tea-img">' +
+                    "<img src=" +
+                    imgI +
+                    "/>" +
+                    "</div>" +
+                    '<p class="tea_title">' +
+                    product.productName +
+                    "</p>" +
+                    '<p class="tea_contect">' +
+                    product.productDesc +
+                    "</p>" +
+                    '<hr class="hr-1">' +
+                    "</li>";
+                // for ( k=0;k<proId.length;k++){
+                // 	console.log(proId[k])
+                // }
+            }
+            $(".product_list>ul").html(html);
+            $(".product_list>ul>li").click(function (e) {
+                console.log("qqqqqqqq");
+                console.log(e.currentTarget.id);
+                window.location.href = "product.html?productId=" + e.currentTarget.id;
+            });
+        },
+        error: function data() {
+            console.log("dataerro", data);
+        }
+    });
 }
 
 $(document).ready(function () {
     console.log("所有chanp!");
     getProductList();
 
-    let openId =  window.localStorage.getItem("openId");
-
-    if (openId === undefined || openId === null){
-        getOpenId()
+    let openId = window.localStorage.getItem("openId");
+    let wx = (function () {
+            return navigator.userAgent.toLowerCase().indexOf('micromessenger') !== -1
+        }
+    )();
+    if (openId === undefined || openId === null) {
+        if (wx) {
+            getOpenId()
+        }
     }
 
 });
@@ -84,31 +89,31 @@ function getOpenId() {
     //code 无值，
     let code = obj.code;
     // 判断openId是否存在
-        $.ajax({
-            url: "https://kidstoms.com/getOpenIdByCode",
-            type: "get",
-            dataType: "json",
-            data: {
-                code:code
-            },
-            success: function(res) {
-                console.log(res);
-                if (res.code == "200") {
-                  console.log(123)
-                    let result = jQuery.parseJSON(res.data);
-                    var openId = result.openId;
-                    console.log(openId);
-                    window.localStorage.setItem("openId", openId);
-                    window.location.href = "H5/product.html?openId=" + openId;
-                    alert("openid:" + result.openId);
-                } else {
-                    alert(res.msg);
-                }
-            },
-            error: function() {
-                console.log("XMLHttpRequest", XMLHttpRequest);
+    $.ajax({
+        url: "https://kidstoms.com/getOpenIdByCode",
+        type: "get",
+        dataType: "json",
+        data: {
+            code: code
+        },
+        success: function (res) {
+            console.log(res);
+            if (res.code == "200") {
+                console.log(123)
+                let result = jQuery.parseJSON(res.data);
+                var openId = result.openId;
+                console.log(openId);
+                window.localStorage.setItem("openId", openId);
+                window.location.href = "H5/product.html?openId=" + openId;
+                alert("openid:" + result.openId);
+            } else {
+                alert(res.msg);
             }
-        });
+        },
+        error: function () {
+            console.log("XMLHttpRequest", XMLHttpRequest);
+        }
+    });
 
 }
 
