3cb21d9c83f46fb9d487bccf4161b532e6862c7a
tomscheney <417497299@qq.com>
2019-12-05 02:49:05 +0800

    kk

--- H5/js/cart.js
+++ H5/js/cart.js
@@ -192,7 +192,6 @@ function getList() {
         console.log(result);
         var htmls = template("addressTpl", { "result": result });
         console.log(htmls);
-        htmls = '<html>hello</html>'
         var obj = $("#address_box>ul");
         console.log(obj);
         var results = $("#address_box>ul").html(htmls);
