96f52d35ebc454388a04756693c984ef124b6220
wangsiyuan <wsy1005@163.com>
2019-12-05 14:29:44 +0800

    修改代码

--- H5/js/cart.js
+++ H5/js/cart.js
@@ -192,7 +192,7 @@ function getList() {
         console.log(result);
         var htmls = template("addressTpl", { result: result });
         console.log(htmls);
-        $(".address-box>.address-boxes").html(htmls);
+        $(".address-box").html(htmls);
         // document.getElementById("template-box").innerHTML = htmls;
       }
     }
