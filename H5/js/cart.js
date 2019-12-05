55fc9885005fc5f74c3336413f52aee0cd78f1a1
wangsiyuan <wsy1005@163.com>
2019-12-05 16:31:12 +0800

    修改代码

--- H5/js/cart.js
+++ H5/js/cart.js
@@ -159,8 +159,9 @@ function koncat() {
 
 // 删除接口 deleteProduct数据
 // 利用事件委托选择到删除按钮所在ul
-$(".address-box").on("click", ".deletedImg", function() {
+$(".address-box").on("tap", ".deletedImg", function() {
   alert(1);
+  console.log("进来删除操作了")
 });
 
 // function DelShop() {
