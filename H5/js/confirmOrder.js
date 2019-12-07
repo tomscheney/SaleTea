a12fcae3ffded285fc6ecd903b07aa0f58afc85a
wangsiyuan <wsy1005@163.com>
2019-12-07 11:53:54 +0800

    修改代码

--- H5/js/confirmOrder.js
+++ H5/js/confirmOrder.js
@@ -1,5 +1,5 @@
 // 进去立即购买页面 调buyNow接口
-$(document).ready(function(productId) {
+function buyNow(productId) {
   $.ajax({
     url: "https://kidstoms.com/buyNow",
     type: "post",
@@ -12,7 +12,6 @@ $(document).ready(function(productId) {
       console.log(res);
       if (res.code === "200") {
         // template("", { res });
-
         // console.log(13121212);
         // location.href =
         //   "confirmOrder.html?totalFee=" +
@@ -22,51 +21,36 @@ $(document).ready(function(productId) {
       }
     }
   });
-});
+}
 
 function ex() {
-    location.href = "exchange.html";
+  location.href = "exchange.html";
 }
 
 function adda() {
-    location.href = "addAddress.html";
+  location.href = "addAddress.html";
 }
 
 function goPay() {
-    $("#btn-car3").click(function () {
-        var istrue = false;
-        var inp = $(".checkbox-list-input");
-        for (var i = 0; i < inp.length; i++) {
-            if (inp.eq(i).prop("checked")) {
-                istrue = true;
-                break;
-            }
-        }
-        if (!istrue) {
-            alert(
-                "该笔订单内包含不可退换货/款的商品。付款前请务必详阅并知晓相关政策，并勾选确认"
-            );
-            return;
-        }
-        // orderNo
-        location.href = "payResult.html?totalFee=" + totalFee + "&orderNo=" + orderNo;
-    });
+  $("#btn-car3").click(function() {
+    var istrue = false;
+    var inp = $(".checkbox-list-input");
+    for (var i = 0; i < inp.length; i++) {
+      if (inp.eq(i).prop("checked")) {
+        istrue = true;
+        break;
+      }
+    }
+    if (!istrue) {
+      alert(
+        "该笔订单内包含不可退换货/款的商品。付款前请务必详阅并知晓相关政策，并勾选确认"
+      );
+      return;
+    }
+    // orderNo
+    location.href =
+      "payResult.html?totalFee=" + totalFee + "&orderNo=" + orderNo;
+  });
 }
 
-$("#liquan").click(function () {
-    if ($(".vou>span:nth-of-type(2)>img:nth-of-type(2)").is(':hidden')) {
-        $(".vou>span:nth-of-type(2)>img:nth-of-type(2)").show();
-        $(".inp").show();
-        $(".vou>span:nth-of-type(2)>img:nth-of-type(1)").hide();
-
-    } else {
-        $(".vou>span:nth-of-type(2)>img:nth-of-type(2)").hide();
-        $(".vou>span:nth-of-type(2)>img:nth-of-type(1)").show();
-        $(".inp").hide();
-    }
-
-})
-
-$(document).ready(function () {
-    buyNow();
-});
+$(document).ready(function() {});
