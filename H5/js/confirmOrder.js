63bc8d4e41393d8dcdcebadb8f4fcb62723deef3
wangsiyuan <wsy1005@163.com>
2019-12-07 11:50:43 +0800

    修改代码

--- H5/js/confirmOrder.js
+++ H5/js/confirmOrder.js
@@ -1,78 +1,5 @@
-// // 进去立即购买页面 调buyNow接口
-// $(document).ready(function() {
-//   $.ajax({
-//     url: "https://kidstoms.com/buyNow",
-//     type: "post",
-//     dataType: "json",
-//     data: {
-//       openId: window.localStorage.getItem("openId"),
-//       productId: productId
-//     },
-//     success: function(res) {
-//       console.log(res);
-//       if (res.code === "200") {
-//         template("", { res });
-
-//         // console.log(13121212);
-//         // location.href =
-//         //   "confirmOrder.html?totalFee=" +
-//         //   res.data.orderAmount +
-//         //   "&orderId=" +
-//         //   res.data.orderId;
-//       }
-//     }
-//   });
-// });
-
-// function ex() {
-//     location.href = "exchange.html";
-// }
-
-// function adda() {
-//     location.href = "addAddress.html";
-// }
-
-// function goPay() {
-//     $("#btn-car3").click(function () {
-//         var istrue = false;
-//         var inp = $(".checkbox-list-input");
-//         for (var i = 0; i < inp.length; i++) {
-//             if (inp.eq(i).prop("checked")) {
-//                 istrue = true;
-//                 break;
-//             }
-//         }
-//         if (!istrue) {
-//             alert(
-//                 "该笔订单内包含不可退换货/款的商品。付款前请务必详阅并知晓相关政策，并勾选确认"
-//             );
-//             return;
-//         }
-//         // orderNo
-//         location.href = "payResult.html?totalFee=" + totalFee + "&orderNo=" + orderNo;
-//     });
-// }
-
-// $("#liquan").click(function () {
-//     if ($(".vou>span:nth-of-type(2)>img:nth-of-type(2)").is(':hidden')) {
-//         $(".vou>span:nth-of-type(2)>img:nth-of-type(2)").show();
-//         $(".inp").show();
-//         $(".vou>span:nth-of-type(2)>img:nth-of-type(1)").hide();
-
-//     } else {
-//         $(".vou>span:nth-of-type(2)>img:nth-of-type(2)").hide();
-//         $(".vou>span:nth-of-type(2)>img:nth-of-type(1)").show();
-//         $(".inp").hide();
-//     }
-
-// })
-
-// $(document).ready(function () {
-//     buyNow();
-// });
-
-$(function(productId) {
-  // 进去立即购买页面 调buyNow接口
+// 进去立即购买页面 调buyNow接口
+$(document).ready(function(productId) {
   $.ajax({
     url: "https://kidstoms.com/buyNow",
     type: "post",
@@ -84,7 +11,7 @@ $(function(productId) {
     success: function(res) {
       console.log(res);
       if (res.code === "200") {
-        template("", { res });
+        // template("", { res });
 
         // console.log(13121212);
         // location.href =
@@ -95,4 +22,51 @@ $(function(productId) {
       }
     }
   });
+});
+
+function ex() {
+    location.href = "exchange.html";
+}
+
+function adda() {
+    location.href = "addAddress.html";
+}
+
+function goPay() {
+    $("#btn-car3").click(function () {
+        var istrue = false;
+        var inp = $(".checkbox-list-input");
+        for (var i = 0; i < inp.length; i++) {
+            if (inp.eq(i).prop("checked")) {
+                istrue = true;
+                break;
+            }
+        }
+        if (!istrue) {
+            alert(
+                "该笔订单内包含不可退换货/款的商品。付款前请务必详阅并知晓相关政策，并勾选确认"
+            );
+            return;
+        }
+        // orderNo
+        location.href = "payResult.html?totalFee=" + totalFee + "&orderNo=" + orderNo;
+    });
+}
+
+$("#liquan").click(function () {
+    if ($(".vou>span:nth-of-type(2)>img:nth-of-type(2)").is(':hidden')) {
+        $(".vou>span:nth-of-type(2)>img:nth-of-type(2)").show();
+        $(".inp").show();
+        $(".vou>span:nth-of-type(2)>img:nth-of-type(1)").hide();
+
+    } else {
+        $(".vou>span:nth-of-type(2)>img:nth-of-type(2)").hide();
+        $(".vou>span:nth-of-type(2)>img:nth-of-type(1)").show();
+        $(".inp").hide();
+    }
+
+})
+
+$(document).ready(function () {
+    buyNow();
 });
