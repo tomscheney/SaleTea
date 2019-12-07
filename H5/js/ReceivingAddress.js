5c83e0b5d445535e78ed16ad0f2469f53736697a
wangsiyuan <wsy1005@163.com>
2019-12-07 15:34:28 +0800

    修改代码

--- H5/js/ReceivingAddress.js
+++ H5/js/ReceivingAddress.js
@@ -1,38 +1,36 @@
+// 保存并返回的时候 调saveAddress接口
+function newAdd() {
+  var userName = $("#name").val(); // 都按这获取
+  var telephone = $("#phone").val();
+  var address = $("#address").val();
+  console.log("用户名", userName);
+  console.log("手机号", telephone);
+  console.log("地址", address);
 
-// // 保存并返回
-// function newAdd() {
-//   var userName = $("#name").val(); // 都按这获取
-//   var telephone = $("#phone").val();
-//   var address = $("#address").val();
-//   console.log("用户名", userName);
-//   console.log("手机号", telephone);
-//   console.log("地址", address);
-
-//   $.ajax({
-//     url: "https://47.112.98.24:8090/saveUser",
-//     type: "post",
-//     // processData: false,
-//     dataType: "json",
-//     contentType: "application/json",
-//     data: JSON.stringify({
-//       userName: userName,
-//       telephone: telephone,
-//       address: address
-//     }),
-//     success: function(data) {
-//       console.log(2);
-//       if (userName != "" && telephone != "" && address != "") {
-//         console.log(data);
-//         console.log(1);
-//         // location.href = "new-add.html";
-//       }
-//     },
-//     error: function(data) {
-//       console.log(103);
-//       // $('#serverResponse').html(data.userName + " : " + data.telephone + " : " + data.address);
-//     }
-//   });
-// }
-// function newADd() {
-//   location.href = "add-address.html";
-// }
+  $.ajax({
+    url: "https://47.112.98.24:8090/saveAddress",
+    type: "post",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify({
+      userName: userName,
+      telephone: telephone,
+      address: address
+    }),
+    success: function(data) {
+      console.log(2);
+      if (userName != "" && telephone != "" && address != "") {
+        console.log(data);
+        console.log(1);
+        // location.href = "new-add.html";
+      }
+    },
+    error: function(data) {
+      console.log(103);
+      // $('#serverResponse').html(data.userName + " : " + data.telephone + " : " + data.address);
+    }
+  });
+}
+function newADd() {
+  location.href = "add-address.html";
+}
