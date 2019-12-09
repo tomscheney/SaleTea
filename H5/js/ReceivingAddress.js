797749029bcd8caeec11ec900d086007a32bc163
wangsiyuan <wsy1005@163.com>
2019-12-09 18:02:06 +0800

    修改代码

--- H5/js/ReceivingAddress.js
+++ H5/js/ReceivingAddress.js
@@ -1,44 +1,45 @@
 // 保存并返回的时候 调saveAddress接口
 function newAdd() {
-  var userName = $("#name").val(); // 都按这获取
-  var telephone = $("#phone").val();
-  var province = $(".select-province").val();
-  var city = $(".select-city").val();
-  var area = $(".select-area").val();
-  var address = $("#address").val();
-  let newAddtess = "province + city + area + address";
-  console.log("用户名", userName);
-  console.log("手机号", telephone);
-  console.log("省", province);
-  console.log("市", city);
-  console.log("区", area);
-  console.log("地址", address);
-  console.log(newAddtess);
+  // var userName = $("#name").val(); // 都按这获取
+  // var telephone = $("#phone").val();
+  // var province = $(".select-province").val();
+  // var city = $(".select-city").val();
+  // var area = $(".select-area").val();
+  // var address = $("#address").val();
+  // let newAddtess = "province + city + area + address";
+  // console.log("用户名", userName);
+  // console.log("手机号", telephone);
+  // console.log("省", province);
+  // console.log("市", city);
+  // console.log("区", area);
+  // console.log("地址", address);
+  // console.log(newAddtess);
 
-  $.ajax({
-    url: "https://47.112.98.24:8090/saveAddress",
-    type: "post",
-    dataType: "json",
-    contentType: "application/json",
-    data: JSON.stringify({
-      userName: userName,
-      telephone: telephone,
-      // addressDetail:,
-      // postcode:,
-      openId: localStorage.getItem("openId")
-    }),
-    success: function(res) {
-      console.log(res);
-      location.href = "addAddress.html";
-      // if (userName != "" && telephone != "" && address != "") {
-      //   // location.href = "new-add.html";
-      // }
-    },
-    error: function(data) {
-      console.log(103);
-      // $('#serverResponse').html(data.userName + " : " + data.telephone + " : " + data.address);
-    }
-  });
+  // $.ajax({
+  //   url: "https://47.112.98.24:8090/saveAddress",
+  //   type: "post",
+  //   dataType: "json",
+  //   contentType: "application/json",
+  //   data: JSON.stringify({
+  //     userName: userName,
+  //     telephone: telephone,
+  //     // addressDetail:,
+  //     // postcode:,
+  //     openId: localStorage.getItem("openId")
+  //   }),
+  //   success: function(res) {
+  //     console.log(res);
+  //     location.href = "addAddress.html";
+  //     // if (userName != "" && telephone != "" && address != "") {
+  //     //   // location.href = "new-add.html";
+  //     // }
+  //   },
+  //   error: function(data) {
+  //     console.log(103);
+  //     // $('#serverResponse').html(data.userName + " : " + data.telephone + " : " + data.address);
+  //   }
+  // });
+  alert(123132);
 }
 function newADd() {
   location.href = "add-address.html";
