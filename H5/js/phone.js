4b9b90552f127818d929cb7a0a2904976f743d37
wangsiyuan <wsy1005@163.com>
2019-12-10 10:56:06 +0800

    修改代码

--- H5/js/phone.js
+++ H5/js/phone.js
@@ -1,17 +1,14 @@
 var InterValObj; //timer变量，控制时间
-var count = 30; //间隔函数，1秒执行
+var count = 60; //间隔函数，1秒执行
 var curCount; //当前剩余秒数
 var code = ""; //验证码
 var codeLength = 6; //验证码长度
 function sendMessage() {
   curCount = count;
   var dealType; //验证方式
-  var uid = $("#uid").val(); //用户uid
   var phone = $("#add_phone").val();
-  if ($("#phone").attr("checked") == true) {
+  if ($("#add_phone").attr("checked") == true) {
     dealType = "phone";
-  } else {
-    dealType = "email";
   }
   //产生验证码
   for (var i = 0; i < codeLength; i++) {
@@ -22,15 +19,15 @@ function sendMessage() {
   $("#btnSendCode").val(+curCount + "秒再获取");
   InterValObj = window.setInterval(SetRemainTime, 1000); //启动计时器，1秒执行一次
   $.ajax({
+    url: "https://kidstoms.com/getSecurityCode",
     type: "post",
-    dataType: "JSON",
-    url: "https://kidstoms.com/getSecurityCode", 
+    dataType: "json",
     data: {
       openId: window.localStorage.getItem("openId"),
       phone: phone
     },
     success: function(res) {
-      alert("验证码:" + res);
+      console.log(res);
     },
     error: function() {}
   });
