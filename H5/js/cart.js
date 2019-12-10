7f4a5646fb333a5efa3f8c5b97f0e1801ba43090
chenjian <chenjian12@jd.com>
2019-12-10 16:28:26 +0800

    jj

--- H5/js/cart.js
+++ H5/js/cart.js
@@ -73,9 +73,7 @@ $(function () {
     });
 
     //单选
-    $(".address-box  .product-box .product-ckb .product-em").on(
-        "click",
-        function () {
+    $(".address-box  .product-box .product-ckb .product-em").click(function () {
             var xz = $(".product-em");
             var xz1 = $(".product-xz");
             if (xz1.length == xz.length) {
