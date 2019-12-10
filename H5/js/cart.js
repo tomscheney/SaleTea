c46a41e5fb757de20e22a23f5e2bd664ad166c5b
tomscheney <417497299@qq.com>
2019-12-10 19:40:56 +0800

    dd

--- H5/js/cart.js
+++ H5/js/cart.js
@@ -73,7 +73,7 @@ $(function () {
     });
 
     //单选
-    $(".product-em ").on('click',function () {
+    $(".product-em").on('click',function () {
             var xz = $(".product-em");
             var xz1 = $(".product-xz");
             if (xz1.length == xz.length) {
