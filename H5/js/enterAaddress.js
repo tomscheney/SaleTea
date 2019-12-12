f1beb59e926d502f9bbbe4347fc52ed7a2a082e6
wangsiyuan <wsy1005@163.com>
2019-12-12 14:53:59 +0800

    修改代码

--- H5/js/enterAaddress.js
+++ H5/js/enterAaddress.js
@@ -1,6 +1,28 @@
+var userName = window.localStorage.getItem("userName");
+var telephone = $("#phone").val();
+var postcode = $("#postcode").val();
+var province = $("#select-province").val();
+var city = $("#select-city").val();
+var area = $("#select-area").val();
+var address = $("#address").val();
+let addressDetail = province + city + area + address;
+console.log("用户名", userName);
+console.log("手机号", telephone);
+console.log("邮编", postcode);
+console.log("省", province);
+console.log("市", city);
+console.log("区", area);
+console.log("地址", address);
+console.log("具体地址", addressDetail);
+
+
 // 保存并返回的时候 调saveAddress接口
 function newAdd() {
-  var userName = window.localStorage.getItem("userName");
+  // let userName = window.localStorage.getItem("userName");
+  // let telephone = window.localStorage.getItem("telephone");
+  // let addressDetail = window.localStorage.getItem("addressDetail");
+  // let postcode = window.localStorage.getItem("postcode");
+  var userName = $("#name").val();
   var telephone = $("#phone").val();
   var postcode = $("#postcode").val();
   var province = $("#select-province").val();
