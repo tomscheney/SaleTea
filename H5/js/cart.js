6390739d3c1691713ca8decd40cf57509b73612b
wangsiyuan <wsy1005@163.com>
2019-12-17 14:13:24 +0800

    修改代码

--- H5/js/cart.js
+++ H5/js/cart.js
@@ -2,270 +2,267 @@ let openId = window.localStorage.getItem("openId");
 let params = location.search.split("=");
 let productId = params[1];
 
-$(function () {
-      //加的效果
-    $(".product-add").click(function () {
-        var n = $(this)
-            .prev()
-            .val();
-        var num = parseInt(n) + 1;
-        if (num == 99) {
-            return;
-        }
-        $(this)
-            .prev()
-            .val(num);
-        TotalPrice();
-    });
+$(function() {
+  //加的效果
+  $(".product-add").click(function() {
+    var n = $(this)
+      .prev()
+      .val();
+    var num = parseInt(n) + 1;
+    if (num == 99) {
+      return;
+    }
+    $(this)
+      .prev()
+      .val(num);
+    TotalPrice();
+  });
 
-    //减的效果
-    $(".product-jian").click(function () {
-        var n = $(this)
-            .next()
-            .val();
-        var num = parseInt(n) - 1;
-        if (num == 0) {
-            return;
-        }
-        $(this)
-            .next()
-            .val(num);
-        TotalPrice();
-    });
+  //减的效果
+  $(".product-jian").click(function() {
+    var n = $(this)
+      .next()
+      .val();
+    var num = parseInt(n) - 1;
+    if (num == 0) {
+      return;
+    }
+    $(this)
+      .next()
+      .val(num);
+    TotalPrice();
+  });
 
-    //删除产品
-    $(".product-ckb").click(function () {
-        $(this)
-            .children("em")
-            .toggleClass("product-xz");
-        TotalPrice();
-        productxz();
-    });
-    //全选产品
-    $(".product-al").click(function () {
-        var fxk = $(".product-em");
-        var qx = $(".product-all em");
-        qx.toggleClass("product-all-on");
-        if (
-            $(this)
-                .find(".product-all em")
-                .is(".product-all-on")
-        ) {
-            fxk.addClass("product-xz");
-        } else {
-            fxk.removeClass("product-xz");
-        }
-        TotalPrice();
-        shuliang();
-    });
-    //删除产品
-    $(".product-del").click(function () {
-        if (confirm("您确定要删除当前商品？")) {
-            $(this)
-                .closest(".product-box")
-                .remove();
-        }
-
-        koncat();
-        TotalPrice();
-        shuliang();
-    });
-
-
+  //删除产品
+  $(".product-ckb").click(function() {
+    $(this)
+      .children("em")
+      .toggleClass("product-xz");
+    TotalPrice();
+    productxz();
+  });
+  //全选产品
+  $(".product-al").click(function() {
+    var fxk = $(".product-em");
+    var qx = $(".product-all em");
+    qx.toggleClass("product-all-on");
+    if (
+      $(this)
+        .find(".product-all em")
+        .is(".product-all-on")
+    ) {
+      fxk.addClass("product-xz");
+    } else {
+      fxk.removeClass("product-xz");
+    }
     TotalPrice();
     shuliang();
-    koncat();
+  });
+  //删除产品
+  $(".product-del").click(function() {
+    if (confirm("您确定要删除当前商品？")) {
+      $(this)
+        .closest(".product-box")
+        .remove();
+    }
 
+    koncat();
+    TotalPrice();
+    shuliang();
+  });
+
+  TotalPrice();
+  shuliang();
+  koncat();
 });
 
 //选中产品
 function selectProduct(index) {
-
-    let elems = document.getElementsByClassName("product-em");
-    let elem = elems[index];
-    if (elem.classList.contains("product-xz")) {
-
-        elem.classList.remove("product-xz");
-    } else {
-        elem.classList.add("product-xz");
-
-    }
-    shuliang();
-    TotalPrice();
+  let elems = document.getElementsByClassName("product-em");
+  let elem = elems[index];
+  if (elem.classList.contains("product-xz")) {
+    elem.classList.remove("product-xz");
+  } else {
+    elem.classList.add("product-xz");
+  }
+  shuliang();
+  TotalPrice();
 }
-
 
 //计算产品价格
 function TotalPrice() {
-    //总价
-    var total = 0;
-    $(".product-em").each(function () {
-        if ($(this).is(".product-xz")) {
-            var price = parseInt(
-                $(this)
-                    .parents(".product-ckb")
-                    .siblings()
-                    .find(".price")
-                    .text()
-            ); //得到产品单价
-            var slproice = parseInt(
-                $(this)
-                    .parents(".product-ckb")
-                    .siblings()
-                    .find(".product-num")
-                    .val()
-            ); //得到产品数量
-            var dgtotal = price * slproice;
-            total += dgtotal;
-        }
-        $(".all-price").text(total.toFixed(2)); //输出全部总价
-    });
+  //总价
+  var total = 0;
+  $(".product-em").each(function() {
+    if ($(this).is(".product-xz")) {
+      var price = parseInt(
+        $(this)
+          .parents(".product-ckb")
+          .siblings()
+          .find(".price")
+          .text()
+      ); //得到产品单价
+      var slproice = parseInt(
+        $(this)
+          .parents(".product-ckb")
+          .siblings()
+          .find(".product-num")
+          .val()
+      ); //得到产品数量
+      var dgtotal = price * slproice;
+      total += dgtotal;
+    }
+    $(".all-price").text(total.toFixed(2)); //输出全部总价
+  });
 }
-
 
 //获取选择产品数量
 function shuliang() {
-    $(".product-all-sl").text("");
-    var cd = $(".product-xz").length;
-    $(".product-all-sl").text(cd);
+  $(".product-all-sl").text("");
+  var cd = $(".product-xz").length;
+  $(".product-all-sl").text(cd);
 
-    if (cd > 0) {
-        $(".product-all-qx").text("已选");
-        $(".all-sl").css("display", "inline-block");
-        $(".product-sett").removeClass("product-sett-a");
-    } else {
-        $(".product-all-qx").text("全选");
-        $(".all-sl").css("display", "none");
-        $(".product-sett").addClass("product-sett-a");
-    }
+  if (cd > 0) {
+    $(".product-all-qx").text("已选");
+    $(".all-sl").css("display", "inline-block");
+    $(".product-sett").removeClass("product-sett-a");
+  } else {
+    $(".product-all-qx").text("全选");
+    $(".all-sl").css("display", "none");
+    $(".product-sett").addClass("product-sett-a");
+  }
 }
 
 //购物车空
 function koncat() {
-    var pic = $(".product-box").length;
-    if (pic <= 0) {
-        $(".all-price").text("0.00");
-        $(".product-all-qx").text("全选");
-        $(".all-sl").css("display", "none");
-        $(".product-sett").addClass("product-sett-a");
-        $(".product-all em").removeClass("product-all-on");
-        $(".kon-cat").css("display", "block");
-    } else {
-        $(".kon-cat").css("display", "none");
-    }
+  var pic = $(".product-box").length;
+  if (pic <= 0) {
+    $(".all-price").text("0.00");
+    $(".product-all-qx").text("全选");
+    $(".all-sl").css("display", "none");
+    $(".product-sett").addClass("product-sett-a");
+    $(".product-all em").removeClass("product-all-on");
+    $(".kon-cat").css("display", "block");
+  } else {
+    $(".kon-cat").css("display", "none");
+  }
 }
 
 // 删除接口 deleteProduct数据
 // 利用事件委托选择到删除按钮所在ul
 function dels(productId) {
-    var r = confirm("您确定要删除当前商品？");
-    if (r == true) {
-        $.ajax({
-            url: "https://kidstoms.com/deleteProduct",
-            type: "post",
-            dataType: "json",
-            data: {
-                openId: localStorage.getItem("openId"),
-                productId: productId
-            },
-            success: function (res) {
-                if (res.code == "200") {
-                    getList();
-                }
-            }
-        });
-    }
+  var r = confirm("您确定要删除当前商品？");
+  if (r == true) {
+    $.ajax({
+      url: "https://kidstoms.com/deleteProduct",
+      type: "post",
+      dataType: "json",
+      data: {
+        openId: localStorage.getItem("openId"),
+        productId: productId
+      },
+      success: function(res) {
+        if (res.code == "200") {
+          getList();
+        }
+      }
+    });
+  }
 }
 
 // 点击购物车  调queryShopCart接口
 function getList() {
-    let openId = localStorage.getItem("openId");
-    // 获取数据渲染数据到页面
-    $.ajax({
-        url: "https://kidstoms.com/queryShopCart",
-        type: "post",
-        dataType: "json",
-        data: {
-            openId: openId
-        },
-        success: function (res) {
-            if (res.code == "200") {
-                var result = res.data.productList;
-                var amounts = res.data.amounts;
-                var htmls = template("addressTpl", {
-                    result: result,
-                    amounts: amounts
-                });
-                $(".address-box").html(htmls);
-                shuliang();
-                TotalPrice();
-                var qx = $(".product-all em");
-                qx.toggleClass("product-all-on");
-            }
-        }
-    });
+  let openId = localStorage.getItem("openId");
+  // 获取数据渲染数据到页面
+  $.ajax({
+    url: "https://kidstoms.com/queryShopCart",
+    type: "post",
+    dataType: "json",
+    data: {
+      openId: openId
+    },
+    success: function(res) {
+      if (res.code == "200") {
+        var result = res.data.productList;
+        var amounts = res.data.amounts;
+        var htmls = template("addressTpl", {
+          result: result,
+          amounts: amounts
+        });
+        $(".address-box").html(htmls);
+        shuliang();
+        TotalPrice();
+        var qx = $(".product-all em");
+        qx.toggleClass("product-all-on");
+      }
+    }
+  });
 }
 
 // 点击减号（-）的时候 调 reduceProductAmount接口
 function reduce(index, productId) {
+  let elements = document.getElementsByClassName("product-num");
+  let ele = elements[index];
+  let value = ele.value;
 
-    let elements = document.getElementsByClassName("product-num");
-    let ele = elements[index]
-    let value = ele.value
-
-    if (value <= 1) {
-        return;
+  if (value <= 1) {
+    return;
+  }
+  $.ajax({
+    url: "https://kidstoms.com/reduceProductAmount",
+    type: "post",
+    dataType: "json",
+    data: {
+      openId: localStorage.getItem("openId"),
+      productId: productId
+    },
+    success: function(res) {
+      let num = parseInt(value) - 1;
+      ele.value = num;
+      TotalPrice();
     }
-    $.ajax({
-        url: "https://kidstoms.com/reduceProductAmount",
-        type: "post",
-        dataType: "json",
-        data: {
-            openId: localStorage.getItem("openId"),
-            productId: productId
-        },
-        success: function (res) {
-
-            let num = parseInt(value) - 1;
-            ele.value = num;
-            TotalPrice()
-        }
-    });
-
+  });
 }
 
 // 点击加号（+）的时候 调 addToShopCart接口
 function add(index, productId) {
-    $.ajax({
-        url: "https://kidstoms.com/addToShopCart",
-        type: "post",
-        dataType: "json",
-        data: {
-            openId: localStorage.getItem("openId"),
-            productId: productId
-        },
-        success: function (res) {
-            if (res.code == "200") {
-                let elements = document.getElementsByClassName("product-num");
-                let ele = elements[index]
-                let value = ele.value
-                let num = parseInt(value) + 1;
+  $.ajax({
+    url: "https://kidstoms.com/addToShopCart",
+    type: "post",
+    dataType: "json",
+    data: {
+      openId: localStorage.getItem("openId"),
+      productId: productId
+    },
+    success: function(res) {
+      if (res.code == "200") {
+        let elements = document.getElementsByClassName("product-num");
+        let ele = elements[index];
+        let value = ele.value;
+        let num = parseInt(value) + 1;
 
-                ele.value = num;
-                TotalPrice()
-
-            }
-        }
-    });
+        ele.value = num;
+        TotalPrice();
+      }
+    }
+  });
 }
 
-function goProductPage(productId){
-    location.href = "product.html?productId=" + productId;
+function goProductPage(productId) {
+  location.href = "product.html?productId=" + productId;
 }
 
-$(document).ready(function () {
-    getList();
+// 点击结算的时候判断手机号是否登录
+function Settlement() {
+  if (
+    window.localStorage.getItem("telephone") == "" ||
+    window.localStorage.getItem("telephone") == null ||
+    window.localStorage.getItem("telephone") == undefined
+  ) {
+    location.href = "phoneChecking.html?productId=" + productId;
+  }
+}
+
+$(document).ready(function() {
+  getList();
 });
-
-
-
--- H5/js/product.js
+++ H5/js/product.js
@@ -9,209 +9,198 @@ console.log(search);
 console.log(list);
 console.log(productId);
 console.log(openId);
-var linkUrl = ''
+var linkUrl = "";
 var descList = null;
 
 // 点击立即购买
 function buyNow(productId) {
-    if (
-        window.localStorage.getItem("telephone") == "" ||
-        window.localStorage.getItem("telephone") == null ||
-        window.localStorage.getItem("telephone") == undefined
-    ) {
-        location.href = "phoneChecking.html?productId=" + productId;
-    } else {
-        location.href = "confirmOrder.html?productId=" + productId;
-    }
+  if (
+    window.localStorage.getItem("telephone") == "" ||
+    window.localStorage.getItem("telephone") == null ||
+    window.localStorage.getItem("telephone") == undefined
+  ) {
+    location.href = "phoneChecking.html?productId=" + productId;
+  } else {
+    location.href = "confirmOrder.html?productId=" + productId;
+  }
 }
 
 // 点击购物车图标
 function shopCart() {
-    location.href =
-        "shopCar.html?openId=" + window.localStorage.getItem("openId");
-    // location.href = "shopCar.html";
+  location.href =
+    "shopCar.html?openId=" + window.localStorage.getItem("openId");
+  // location.href = "shopCar.html";
 }
 
 // 点击加入购物车 调addToShopCart接口
 function addShopCart() {
-    let params = location.search.split("=");
-    let productId = params[1];
-    $.ajax({
-        url: "https://kidstoms.com/addToShopCart",
-        type: "post",
-        dataType: "json",
-        data: {
-            openId: window.localStorage.getItem("openId"),
-            // productId:location.search.substr(1, 8)
-            productId: productId.substr(0, 8)
-        },
-        success: function (res) {
-            if (res.code === "200") {
-            }
-        }
-    });
+  let params = location.search.split("=");
+  let productId = params[1];
+  $.ajax({
+    url: "https://kidstoms.com/addToShopCart",
+    type: "post",
+    dataType: "json",
+    data: {
+      openId: window.localStorage.getItem("openId"),
+      // productId:location.search.substr(1, 8)
+      productId: productId.substr(0, 8)
+    },
+    success: function(res) {
+      if (res.code === "200") {
+      }
+    }
+  });
 }
 
 // 调用轮播图
 function swiper_init() {
-    var mySwiper = new Swiper(".swiper-container", {
-        observer: true, //修改swiper自己或子元素时，自动初始化swiper
-        observeParents: true, //修改swiper的父元素时，自动初始化swiper
-        loop: true, // 循环模式选项
-        autoplay: true, //等同于以下设置
-        pagination: {
-            el: ".swiper-pagination" // 分页器
-        }
-    });
+  var mySwiper = new Swiper(".swiper-container", {
+    observer: true, //修改swiper自己或子元素时，自动初始化swiper
+    observeParents: true, //修改swiper的父元素时，自动初始化swiper
+    loop: true, // 循环模式选项
+    autoplay: true, //等同于以下设置
+    pagination: {
+      el: ".swiper-pagination" // 分页器
+    }
+  });
 }
 
 // 点击显示 添加数据到接口
-$("#btn-car2").click(function () {
-    AddShop(); //添加产品
-    $(".yuan").show();
-    $("#num").show();
-    $(".jia1").show(100);
-    $(".jia1")
-        .show()
-        .delay(1000)
-        .fadeOut();
+$("#btn-car2").click(function() {
+  AddShop(); //添加产品
+  $(".yuan").show();
+  $("#num").show();
+  $(".jia1").show(100);
+  $(".jia1")
+    .show()
+    .delay(1000)
+    .fadeOut();
 });
 
 // 获取产品详情  getProductDetail接口
 function getProductDetail() {
-    $.ajax({
-        url: "https://kidstoms.com/getProductDetail",
-        type: "post",
-        dataType: "json",
-        data: {
-            productId: productId
-        },
-        success: function (res) {
-            descList = res.data.productRecommend;
+  $.ajax({
+    url: "https://kidstoms.com/getProductDetail",
+    type: "post",
+    dataType: "json",
+    data: {
+      productId: productId
+    },
+    success: function(res) {
+      descList = res.data.productRecommend;
 
-            // 商品简介
-            var recommendDesc = res.data.productRecommend[0].recommendDesc;
-            window.localStorage.setItem("recommendDesc", recommendDesc);
+      // 商品简介
+      var recommendDesc = res.data.productRecommend[0].recommendDesc;
+      window.localStorage.setItem("recommendDesc", recommendDesc);
 
-            linkUrl = res.data.productImages[0];
-            // 标题 价格
-            $(".tit-name").text(res.data.productDesc);
-            $(".tit-price").text(res.data.salePrice + "元 / 份");
-            // 产品信息
-            $(".productPlace").text(res.data.productDetail.productPlace);
-            $(".wrapping").text(res.data.productDetail.wrapping);
-            $(".brand").text(res.data.productDetail.brand);
-            var len = [];
-            len = res.data.productImages;
-            var html = "";
-            for (var i = 0; i < len.length; i++) {
-                var imgI = len[i];
-                html +=
-                    '<div class="swiper-slide">' + "<img src=" + imgI + "/>" + "</div>";
-            }
-            $(".swiper-wrapper").html(html);
-            // 动态获取数据后 渲染插件
-            swiper_init();
-            $(".qualityPeriod").text(res.data.productDetail.qualityPeriod);
-            $(".standards").text(res.data.productDetail.standards);
-            // 产品亮点
-            var fea = [];
-            fea = res.data.productFeatures;
-            var featur = template("featureTpl",{result:fea})
-            $(".bling-spot").after(featur);
+      linkUrl = res.data.productImages[0];
+      // 标题 价格
+      $(".tit-name").text(res.data.productDesc);
+      $(".tit-price").text(res.data.salePrice + "元 / 份");
+      // 产品信息
+      $(".productPlace").text(res.data.productDetail.productPlace);
+      $(".wrapping").text(res.data.productDetail.wrapping);
+      $(".brand").text(res.data.productDetail.brand);
+      var len = [];
+      len = res.data.productImages;
+      var html = "";
+      for (var i = 0; i < len.length; i++) {
+        var imgI = len[i];
+        html +=
+          '<div class="swiper-slide">' + "<img src=" + imgI + "/>" + "</div>";
+      }
+      $(".swiper-wrapper").html(html);
+      // 动态获取数据后 渲染插件
+      swiper_init();
+      $(".qualityPeriod").text(res.data.productDetail.qualityPeriod);
+      $(".standards").text(res.data.productDetail.standards);
+      // 产品亮点
+      var fea = [];
+      fea = res.data.productFeatures;
+      var featur = template("featureTpl", { result: fea });
+      $(".bling-spot").after(featur);
 
-            // 猜你喜欢
-            var productRecommend =  res.data.productRecommend;
-            let recomend = template("recommendTpl",{result:productRecommend})
-            $(".like-spot").after(recomend);
-
-        }
-    });
+      // 猜你喜欢
+      var productRecommend = res.data.productRecommend;
+      let recomend = template("recommendTpl", { result: productRecommend });
+      $(".like-spot").after(recomend);
+    }
+  });
 }
 
-
-$(document).ready(function () {
-
+$(document).ready(function() {
   getProductDetail();
-
 });
 
 // 点击猜你喜欢按钮 调getProductByDesc接口
 function getRecommendProductId(index) {
-
-  let  recommendDesc = descList[index].recommendDesc;
-    $.ajax({
-        url: "https://kidstoms.com/getProductByDesc",
-        type: "post",
-        dataType: "json",
-        data: {
-            desc: recommendDesc
-        },
-        success: function (res) {
-            if (res.data != null) {
-                var productIdDesc = res.data.productId;
-                window.localStorage.setItem("productIdDesc", productIdDesc);
-                location.href = "product.html?productIdDesc=" + productIdDesc;
-            }
-        }
-    });
+  let recommendDesc = descList[index].recommendDesc;
+  $.ajax({
+    url: "https://kidstoms.com/getProductByDesc",
+    type: "post",
+    dataType: "json",
+    data: {
+      desc: recommendDesc
+    },
+    success: function(res) {
+      if (res.data != null) {
+        var productIdDesc = res.data.productId;
+        window.localStorage.setItem("productIdDesc", productIdDesc);
+        location.href = "product.html?productIdDesc=" + productIdDesc;
+      }
+    }
+  });
 }
 
-$(function () {
-    wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
-        wx.updateAppMessageShareData({
-            title: '芷贤斋精选', // 分享标题
-            desc: '芷贤斋精选礼品,精心为您呈上', // 分享描述
-            link: 'https://kidstoms.com/tea/H5/product.html?productId=' + productId, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: linkUrl, // 分享图标
-            success: function () {
-                // 设置成功
-                alert('分享成功')
-            },
-            cancel: function () {
-                alert('分享取消')
-
-            },
-            fail: function () {
-                alert('分享失败')
-
-            },
-            complete: function () {
-                alert('分享完成')
-
-            },
-            trigger: function () {
-                alert('点击了分享菜单')
-
-            }
-        })
+$(function() {
+  wx.ready(function() {
+    //需在用户可能点击分享按钮前就先调用
+    wx.updateAppMessageShareData({
+      title: "芷贤斋精选", // 分享标题
+      desc: "芷贤斋精选礼品,精心为您呈上", // 分享描述
+      link: "https://kidstoms.com/tea/H5/product.html?productId=" + productId, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+      imgUrl: linkUrl, // 分享图标
+      success: function() {
+        // 设置成功
+        alert("分享成功");
+      },
+      cancel: function() {
+        alert("分享取消");
+      },
+      fail: function() {
+        alert("分享失败");
+      },
+      complete: function() {
+        alert("分享完成");
+      },
+      trigger: function() {
+        alert("点击了分享菜单");
+      }
     });
+  });
 
-    wx.ready(function () {      //需在用户可能点击分享按钮前就先调用
-        wx.updateTimelineShareData({
-            title: '芷贤斋精选', // 分享标题
-            link: 'https://kidstoms.com/tea/H5/product.html?productId=' + productId, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: linkUrl, // 分享图标
-            success: function () {
-                // 设置成功
-                alert('分享成功')
-            },
-            cancel: function () {
-                alert('分享取消')
-
-            },
-            fail: function () {
-                alert('分享失败')
-
-            },
-            complete: function () {
-                alert('分享完成')
-
-            },
-            trigger: function () {
-                alert('点击了分享菜单')
-
-            }
-        })
+  wx.ready(function() {
+    //需在用户可能点击分享按钮前就先调用
+    wx.updateTimelineShareData({
+      title: "芷贤斋精选", // 分享标题
+      link: "https://kidstoms.com/tea/H5/product.html?productId=" + productId, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+      imgUrl: linkUrl, // 分享图标
+      success: function() {
+        // 设置成功
+        alert("分享成功");
+      },
+      cancel: function() {
+        alert("分享取消");
+      },
+      fail: function() {
+        alert("分享失败");
+      },
+      complete: function() {
+        alert("分享完成");
+      },
+      trigger: function() {
+        alert("点击了分享菜单");
+      }
     });
-})
+  });
+});
