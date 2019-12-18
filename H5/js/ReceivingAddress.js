5822243aac6412acb56a0628f80c16bc8d54d5bf
wangsiyuan <wsy1005@163.com>
2019-12-18 12:06:11 +0800

    修改代码

--- H5/js/ReceivingAddress.js
+++ H5/js/ReceivingAddress.js
@@ -1,3 +1,13 @@
+
+  var userName = $("#name").val();
+  var telephone = $("#phone").val();
+  var postcode = $("#postcode").val();
+  var addressDetail = $("#address").val();
+  console.log("用户名", userName);
+  console.log("手机号", telephone);
+  console.log("邮编", postcode);
+  console.log("地址", addressDetail);
+
 // 保存并返回的时候 调saveAddress接口
 function newAdd() {
   var userName = $("#name").val();
@@ -21,9 +31,8 @@ function newAdd() {
       openId: localStorage.getItem("openId")
     },
     success: function(res) {
-      console.log(res)
-        location.href = "addAddress.html";
-      
+      console.log(res);
+      location.href = "addAddress.html";
     }
   });
 }
