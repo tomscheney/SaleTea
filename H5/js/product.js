0f94257bb275d8181260411aa4613da89b0e6dba
wangsiyuan <wsy1005@163.com>
2019-12-18 10:29:49 +0800

    修改代码

--- H5/js/product.js
+++ H5/js/product.js
@@ -161,18 +161,19 @@ $(function() {
     },
     dataType: "json",
     success: function(res) {
-      console.log(res)
+      console.log(res);
       wx.config({
         debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: res.data.appId, // 必填，公众号的唯一标识
         timestamp: res.data.timestamp, // 必填，生成签名的时间戳
         nonceStr: res.data.nonceStr, // 必填，生成签名的随机串
-        signature: res.data.signature,// 必填，签名
-        jsApiList: [
-          "onMenuShareTimeline",
-          "onMenuShareQQ",
-          "onMenuShareAppMessage"
-        ] // 必填，需要使用的JS接口列表
+        signature: res.data.signature, // 必填，签名
+        jsapi_ticket: res.data.jsapi_ticket
+        // jsApiList: [
+        //   "onMenuShareTimeline",
+        //   "onMenuShareQQ",
+        //   "onMenuShareAppMessage"
+        // ] // 必填，需要使用的JS接口列表
       });
     }
   });
