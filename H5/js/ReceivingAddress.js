d7d037310506c61a931293843b01f01bd5d2dd76
wangsiyuan <wsy1005@163.com>
2019-12-18 16:29:58 +0800

    修改代码

--- H5/js/ReceivingAddress.js
+++ H5/js/ReceivingAddress.js
@@ -1,12 +1,11 @@
-
-  var userName = $("#name").val();
-  var telephone = $("#phone").val();
-  var postcode = $("#postcode").val();
-  var addressDetail = $("#address").val();
-  console.log("用户名", userName);
-  console.log("手机号", telephone);
-  console.log("邮编", postcode);
-  console.log("地址", addressDetail);
+var userName = $("#name").val();
+var telephone = $("#phone").val();
+var postcode = $("#postcode").val();
+var addressDetail = $("#address").val();
+console.log("用户名", userName);
+console.log("手机号", telephone);
+console.log("邮编", postcode);
+console.log("地址", addressDetail);
 
 // 保存并返回的时候 调saveAddress接口
 function newAdd() {
@@ -19,22 +18,30 @@ function newAdd() {
   console.log("邮编", postcode);
   console.log("地址", addressDetail);
 
-  $.ajax({
-    url: "https://kidstoms.com/saveAddress",
-    type: "post",
-    dataType: "json",
-    data: {
-      userName: userName,
-      telephone: telephone,
-      addressDetail: addressDetail,
-      postcode: postcode,
-      openId: localStorage.getItem("openId")
-    },
-    success: function(res) {
-      console.log(res);
-      location.href = "addAddress.html";
-    }
-  });
+  if (
+    this.userName == "" ||
+    this.userName == null ||
+    this.userName == undefined
+  ) {
+    alert("请添加收货人姓名");
+  } else {
+    $.ajax({
+      url: "https://kidstoms.com/saveAddress",
+      type: "post",
+      dataType: "json",
+      data: {
+        userName: userName,
+        telephone: telephone,
+        addressDetail: addressDetail,
+        postcode: postcode,
+        openId: localStorage.getItem("openId")
+      },
+      success: function(res) {
+        console.log(res);
+        location.href = "addAddress.html";
+      }
+    });
+  }
 }
 
 // 点击返回按钮
