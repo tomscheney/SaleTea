e3b764cebd7a1118edb060d3f3f9e87e41b51fcf
wangsiyuan <wsy1005@163.com>
2019-12-18 10:17:55 +0800

    修改代码

--- H5/js/product.js
+++ H5/js/product.js
@@ -153,6 +153,30 @@ function getRecommendProductId(index) {
 }
 
 $(function() {
+  $.ajax({
+    url: "https://kidstoms.com/getShareConfiguration",
+    type: "post",
+    data: {
+      shareUrl: "https://kidstoms.com/tea/H5/product.html?productId=88880008" // 获取页面当前链接
+    },
+    dataType: "json",
+    success: function(res) {
+      console.log(res)
+      // wx.config({
+      //   debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      //   appId: data["appid"], // 必填，公众号的唯一标识
+      //   timestamp: data["timestamp"], // 必填，生成签名的时间戳
+      //   nonceStr: data["nonceStr"], // 必填，生成签名的随机串
+      //   signature: data["signature"], // 必填，签名
+      //   jsApiList: [
+      //     "onMenuShareTimeline",
+      //     "onMenuShareQQ",
+      //     "onMenuShareAppMessage"
+      //   ] // 必填，需要使用的JS接口列表
+      // });
+    }
+  });
+
   wx.ready(function() {
     //需在用户可能点击分享按钮前就先调用
     wx.updateAppMessageShareData({
