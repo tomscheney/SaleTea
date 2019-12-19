23e0522845997d598a7cc14562897da618c51b50
wangsiyuan <wsy1005@163.com>
2019-12-19 14:50:51 +0800

    修改代码

--- H5/js/product.js
+++ H5/js/product.js
@@ -25,6 +25,41 @@ var descList = null;
 
 // 点击立即购买
 function buyNow(productId) {
+  let wx = (function() {
+    return navigator.userAgent.toLowerCase().indexOf("micromessenger") !== -1;
+  })();
+  if (wx) {
+    let appid = "wx6e974f12e898a2ee";
+    let redirect_uri = "https://kidstoms.com/tea/H5/home.html";
+    let response_type = "code";
+    let state = "STATE#wechat_redirect";
+    let scope = "snsapi_base";
+    let result =
+      "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" +
+      appid +
+      "&redirect_uri=" +
+      redirect_uri +
+      "&response_type=" +
+      response_type +
+      "&scope=" +
+      scope +
+      "&state=" +
+      state;
+    window.location.href = result;
+  } else {
+    // window.location.href = "H5/home.html";
+
+    if (
+      window.localStorage.getItem("telephone") == "" ||
+      window.localStorage.getItem("telephone") == null ||
+      window.localStorage.getItem("telephone") == undefined
+    ) {
+      location.href = "phoneChecking.html?";
+    } else {
+      location.href = "confirmOrder.html?productId=" + productId;
+    }
+  }
+
   if (
     window.localStorage.getItem("telephone") == "" ||
     window.localStorage.getItem("telephone") == null ||
@@ -40,7 +75,6 @@ function buyNow(productId) {
 function shopCart() {
   location.href =
     "shopCar.html?openId=" + window.localStorage.getItem("openId");
-  // location.href = "shopCar.html";
 }
 
 // 点击加入购物车 调addToShopCart接口
@@ -57,8 +91,7 @@ function addShopCart() {
       productId: productId.substr(0, 8)
     },
     success: function(res) {
-      if (res.code === "200") {
-      }
+      console.log(res);
     }
   });
 }
@@ -246,7 +279,6 @@ $(function() {
     });
   });
 });
-
 
 $(document).ready(function() {
   getProductDetail();
