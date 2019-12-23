013705170b1eb26848c37bab97264f9bc3c6523d
wangsiyuan <wsy1005@163.com>
2019-12-23 17:04:54 +0800

    修改代码

--- H5/js/product.js
+++ H5/js/product.js
@@ -284,7 +284,7 @@ $(function() {
 });
 
 $(document).ready(function() {
-  window.location.reload();
+  // window.location.reload();
   getList();
   $("#spanNum").html(window.localStorage.getItem("Length"));
   getProductDetail();
